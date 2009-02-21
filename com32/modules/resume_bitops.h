e6a84c5c3e07fb731e966d64533818188e7efd53
Pierre-Alexandre Meyer <pierre@mouraf.org>
2009-02-21 14:34:23 -0800

    fast-boot: Fixing C99 inline warnings
    Impact: None.
    
    C99 likes only static inline declarations.
    
    Testing Done: checked absence of warnings and ran locally the parser.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- com32/modules/resume_bitops.h
+++ com32/modules/resume_bitops.h
@@ -14,10 +14,80 @@
 #ifndef RESUME_BITOPS_H
 #define RESUME_BITOPS_H
 
-int test_bit(int, volatile const unsigned long*);
-void clear_bit(int, volatile unsigned long*);
-void set_bit(int, volatile unsigned long*);
 void showbits(unsigned long *);
-unsigned long __ffs(unsigned long);
+
+/**
+ * test_bit - test a bit in an address
+ * @nr:		Bit to test.
+ * @addr:	Address to start counting from.
+ **/
+static __inline__ int test_bit(int nr, volatile const unsigned long *addr)
+{
+	return 1UL & (addr[BIT_WORD(nr)] >> (nr & (BITS_PER_LONG-1)));
+}
+
+/**
+ * clear_bit - unset a bit in an address
+ * @bit:	Bit to clear.
+ * @addr:	Address to modify.
+ **/
+static __inline__ void clear_bit(int nr, volatile unsigned long *addr)
+{
+	unsigned long mask = BIT_MASK(nr);
+	unsigned long *p = ((unsigned long *)addr) + BIT_WORD(nr);
+
+	*p &= ~mask;
+}
+
+/**
+ * set_bit - set a bit in memory
+ * @nr:		The bit to set.
+ * @addr:	The address to start counting from.
+ **/
+static __inline__ void set_bit(int nr, volatile unsigned long *addr)
+{
+	unsigned long mask = BIT_MASK(nr);
+	unsigned long *p = ((unsigned long *)addr) + BIT_WORD(nr);
+
+	*p  |= mask;
+}
+
+/**
+ * __ffs - find first bit in word.
+ * @word:	The word to search.
+ *
+ * Undefined if no bit exists, so code should check against 0 first.
+ */
+static __inline__ unsigned long __ffs(unsigned long word)
+{
+	int num = 0;
+
+#if BITS_PER_LONG == 64
+	if ((word & 0xffffffff) == 0) {
+		num += 32;
+		word >>= 32;
+	}
+#endif
+	if ((word & 0xffff) == 0) {
+		num += 16;
+		word >>= 16;
+	}
+	if ((word & 0xff) == 0) {
+		num += 8;
+		word >>= 8;
+	}
+	if ((word & 0xf) == 0) {
+		num += 4;
+		word >>= 4;
+	}
+	if ((word & 0x3) == 0) {
+		num += 2;
+		word >>= 2;
+	}
+	if ((word & 0x1) == 0)
+		num += 1;
+	return num;
+}
+
 
 #endif /* RESUME_BITOPS_H */
