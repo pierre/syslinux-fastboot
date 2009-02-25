91a4c3af6f17264f3268e181c151cacd0f427ba8
Pierre-Alexandre Meyer <pierre@mouraf.org>
2009-02-25 14:33:40 -0800

    fast-boot: Fix symbols loading
    Impact: None.
    
    Refactoring and bugs fixing of the symbol parser.
    
    Testing Done: built the com32 module and ran it.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- com32/modules/resume_symbols.c
+++ com32/modules/resume_symbols.c
@@ -20,37 +20,22 @@
 #include "resume_debug.h"
 
 /* Pointer to the beginning of the image file */
-char* symbols_table_buffer;
+static char* symbols_table_buffer;
 
 /* Current offset */
-unsigned long symbols_table_posn;
+static unsigned long symbols_table_posn;
 
 /* Size of System.map */
-size_t data_len;
+static size_t data_len;
 
-struct swsusp_symbl_info {
-	char		symbl_name[50];
-	unsigned long	*value;
-};
-
-unsigned long saved_context_ebx;
-unsigned long saved_context_esp;
-unsigned long saved_context_ebp;
-unsigned long saved_context_esi;
-unsigned long saved_context_edi;
-unsigned long saved_context_context;
-unsigned long saved_context_eflags;
-unsigned long saved_context_nosave_begin;
-unsigned long saved_context_nosave_end;
-
-static struct swsusp_symbl_info sym_info[] =
+struct swsusp_symbl_info sym_info[] =
 {
     {"saved_context_ebx",       &saved_context_ebx},
     {"saved_context_esp",       &saved_context_esp},
     {"saved_context_ebp",       &saved_context_ebp},
     {"saved_context_esi",       &saved_context_esi},
     {"saved_context_edi",       &saved_context_edi},
-    {"saved_context",           &saved_context_context},
+    {"saved_context",           &saved_context_state},
     {"saved_context_eflags",    &saved_context_eflags},
     {"__nosave_begin",          &saved_context_nosave_begin},
     {"__nosave_end",            &saved_context_nosave_end},
@@ -91,25 +76,28 @@ static int load_symbols_table(void)
  **/
 static int get_kernel_symbl(void)
 {
-	int address_size = 8 * sizeof(char);
-	char *s_address = malloc(address_size + 1);
+	int address_size = 7 * sizeof(char);
+	char s_address[7];
 	unsigned long address = 0;
 	char symbol_found_name[50] = "";
 	int i = 0;
 
-	/* Get adress value */
-	memcpy(s_address, symbols_table_buffer, address_size);
-	s_address[address_size] = '\0';
-	symbols_table_buffer += address_size;
+	/*
+	 * Get adress value
+	 * The address starts with 'c'
+	 */
+	symbols_table_posn++;
+
+	memcpy(s_address, symbols_table_buffer + symbols_table_posn,
+	       address_size);
 	symbols_table_posn += address_size;
 	address = strtol(s_address, NULL, 16);
 #ifdef METADATA_DEBUG
-	dprintf("%s", s_address);
+	dprintf("%lx", address);
 #endif /* METADATA_DEBUG */
 	free(s_address);
 
 	/* Skip symbol type */
-	symbols_table_buffer += 3 * sizeof(char);
 	symbols_table_posn += 3 * sizeof(char);
 #ifdef METADATA_DEBUG
 	dprintf(" ");
@@ -117,17 +105,16 @@ static int get_kernel_symbl(void)
 
 	/* Get symbol name */
 	// XXX Optimize me... The symbols are often at the very end.
-	while (*symbols_table_buffer != '\n') {
-		symbol_found_name[i++] = *symbols_table_buffer;
-		symbols_table_buffer++;
+	while (*(symbols_table_buffer + symbols_table_posn) != '\n') {
+		symbol_found_name[i++] = *(symbols_table_buffer +
+					   symbols_table_posn);
 		symbols_table_posn++;
 		if (i >= 49) {
 			// PCI related stuff.
 			//printf("BUG: Symbol name is longer than 49 chars.\n");
-			while (*symbols_table_buffer != '\n') {
-				symbols_table_buffer++;
+			while (* (symbols_table_buffer +
+				  symbols_table_posn) != '\n')
 				symbols_table_posn++;
-			}
 			return 0;
 		}
 	}
@@ -136,13 +123,12 @@ static int get_kernel_symbl(void)
 #ifdef METADATA_DEBUG
 	dprintf("%s\n", symbol_found_name);
 #endif /* METADATA_DEBUG */
-	symbols_table_buffer++;
 	symbols_table_posn++;
 	i = 0;
 	while (sym_info[i].symbl_name[0] != 0){
 		if (strcmp(sym_info[i].symbl_name, symbol_found_name) == 0) {
 			*(sym_info[i].value) = address;
-			dprintf("Symbol %s is located at 0x%08x.\n",
+			dprintf("Symbol %s is located at 0x%08lx.\n",
 				symbol_found_name, address);
 			return 1;
 		}
@@ -170,9 +156,11 @@ int get_missing_symbols_from_saved_kernel(void)
 			symbols_to_match--;
 	}
 
-	/* 
+	/*
 	 * Don't bother free'ing the memory, we are about to rewrite the Memory
 	 * Map anyway.
+	 * XXX Actually, if I don't free it, it crashes!
 	 */
+	free(symbols_table_buffer);
 	return symbols_to_match;
 }
--- com32/modules/resume_symbols.h
+++ com32/modules/resume_symbols.h
@@ -14,6 +14,47 @@
 #ifndef _RESUME_SYMBOLS_H
 #define _RESUME_SYMBOLS_H
 
+#include "resume_linux.h"
+
+struct swsusp_symbl_info {
+	char		symbl_name[50];
+	unsigned long	*value;
+};
+
+/* See arch/x86/include/asm/desc_defs.h */
+struct desc_ptr {
+	unsigned short size;
+	unsigned long address;
+} __attribute__((packed)) ;
+
+/*
+ * Image of the saved processor state
+ * See arch/x86/include/asm/suspend_32.h
+ */
+struct saved_context {
+	u16 es, fs, gs, ss;
+	unsigned long cr0, cr2, cr3, cr4;
+	struct desc_ptr gdt;
+	struct desc_ptr idt;
+	u16 ldt;
+	u16 tss;
+	unsigned long tr;
+	unsigned long safety;
+	unsigned long return_address;
+} __attribute__((packed));
+
+unsigned long saved_context_ebx;
+unsigned long saved_context_esp;
+unsigned long saved_context_ebp;
+unsigned long saved_context_esi;
+unsigned long saved_context_edi;
+unsigned long saved_context_state;
+unsigned long saved_context_eflags;
+unsigned long saved_context_nosave_begin;
+unsigned long saved_context_nosave_end;
+
+struct swsusp_symbl_info sym_info[10];
+
 int get_missing_symbols_from_saved_kernel(void);
 
 #endif /* _RESUME_SYMBOLS_H */
