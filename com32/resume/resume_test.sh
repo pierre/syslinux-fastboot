494777e4b378a02a129777b3c0364611f6c02ece
Pierre-Alexandre Meyer <pierre@mouraf.org>
2009-04-11 18:34:13 -0700

    fast-boot: Improve testing scripts
    Impact: New 'create_hibernate_image' make target
    
    Some refactoring to automate the testing cycle:
    
    `make test' is still used to test resume.c32.
    `make create_hibernate_image' is used to create the hibernate image.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- com32/resume/resume_test.sh
+++ com32/resume/resume_test.sh
@@ -12,60 +12,109 @@
 #
 # -----------------------------------------------------------------------
 
-# Run `bash resume_test.sh vmx' to test with VMware Workstation using the VIX
-# API. Default behavior is to run with Qemu.
-if [[ $1 == "vmw" ]]; then
-	VMW=true
-fi
+set -e
 
-MNT_PNT=/media/vmx
+# Set VMW to test with VMware Workstation using the VIX
+# API. Default behavior is to run with Qemu.
+#VMW=true
+
+# Global parameters
+VM_ROOT=resume_testing/linux_vm
+MNT_PNT=resume_testing/mnt
+SERIAL=$VM_ROOT/serial.pipe
+
+# Testing parameters
+KERNEL="$VM_ROOT"/bzImage
+RAM_SIZE=1024
+TOI_FILE_OFFSET=0x8960
 
 if [[ -n $VMW ]]; then
 	VMX=/vms/TestsHDD/TestsHDD.vmx
 	VMDK=/vms/TestsHDD/TestsHDD.vmdk
 	trap "sudo vmware-mount -x;vmrun stop $VMX" SIGINT SIGTERM
 else
-	RAW=resume_testing/resume_testing.img
+	RAW=$VM_ROOT/debian_lenny_i386_small.img
+	OFFSET=$((63*512))
+	QEMU="qemu -no-kqemu -m $RAM_SIZE -hda $RAW -serial file:$SERIAL"
+	trap "exit 1" SIGINT SIGTERM
 fi
 
-# Check if the build is valid
-make clean && make resume.c32
-if [[ $? != 0 ]]; then
-	exit 1
-fi
+function test_resume_c32
+{
+	make resume.c32
+	update_resume_c32
 
+	if [[ -n $VMW ]]; then
+		rm -f /tmp/TestsHDD3
+		echo "  VMW     $RAW"
+		vmrun start "$VMX" nogui
+		sleep 2
+		socat unix-connect:/tmp/TestsHDD3 stdout
+		#socat unix-connect:/tmp/TestsHDD3 tcp4-listen:12345
+		vmrun list
+	else
+		echo "  QEMU    $RAW"
+		$QEMU
+	fi
+}
+
+function create_hibernate_image
+{
+	echo "  QEMU    $RAW"
+	$QEMU	-append "resume=file:/dev/sda1:$TOI_FILE_OFFSET console=tty0 console=ttyS0 root=/dev/sda1" \
+		-kernel "$KERNEL"
+
+	# Create the hibernate image by... hibernating the VM!
+	# e.g. echo /image > /sys/power/tuxonice/file/target
+	#      echo 1 > /sys/power/tuxonice/do_hibernate
+
+	update_system_map
+}
+
+function mount_vm
+{
 if [[ -n $VMW ]]; then
-	# Update the .vmdk file
 	echo "  MOUNT   $MNT_PNT"
 	sudo vmware-mount "$VMDK" "$MNT_PNT"
-	echo "  CP      $MNT_PNT/resume.c32"
-	sudo cp resume.c32 "$MNT_PNT"
+else
+	echo "  LOSETUP /dev/loop0"
+	sudo losetup -o "$OFFSET" /dev/loop0 "$RAW"
+	echo "  MOUNT   $MNT_PNT"
+	sudo mount /dev/loop0 "$MNT_PNT"
+fi
+}
+
+function umount_vm
+{
+if [[ -n $VMW ]]; then
 	echo "  UMOUNT  $MNT_PNT"
 	sudo vmware-mount -x
 else
-	echo "  LOSETUP /dev/loop0"
-	sudo losetup /dev/loop0 "$RAW"
-	echo "  MOUNT   $MNT_PNT"
-	sudo mount /dev/loop0 "$MNT_PNT"
-	echo "  CP      $MNT_PNT/resume.c32"
-	sudo cp resume.c32 "$MNT_PNT"
 	echo "  UMOUNT  $MNT_PNT"
 	sudo umount "$MNT_PNT"
 	echo "  LOSETUP /dev/loop0"
 	sudo losetup -d /dev/loop0
 fi
+}
 
-# Run a VM
-if [[ -n $VMW ]]; then
-	rm -f /tmp/TestsHDD3
-	echo "  VMW     $RAW"
-	vmrun start /vms/TestsHDD/TestsHDD.vmx nogui
-	sleep 2
-	socat unix-connect:/tmp/TestsHDD3 stdout
-	#socat unix-connect:/tmp/TestsHDD3 tcp4-listen:12345 
-	vmrun list
+function update_resume_c32
+{
+	mount_vm
+	echo "  CP      $MNT_PNT/resume.c32"
+	sudo cp resume.c32 "$MNT_PNT"
+	umount_vm
+}
+
+function update_system_map
+{
+	mount_vm
+	echo "  CP      $MNT_PNT/System.map"
+	sudo cp "$VM_ROOT"/System.map "$MNT_PNT"
+	umount_vm
+}
+
+if [[ $1 == 'create' ]]; then
+	create_hibernate_image
 else
-	#qemu -nographic -M pc -hda "$RAW" -m 1500 -no-kqemu -boot c #-S -s
-	echo "  QEMU    $RAW"
-	qemu -M pc -hda "$RAW" -m 1500 -no-kqemu -boot c #-S -s
+	test_resume_c32
 fi
